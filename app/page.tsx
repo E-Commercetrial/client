fd5485f84f3a45d4ae997154ab34d77deded5ffe
Tunacodin <bostancibasituna58@gmail.com>
2023-06-27 23:13:07 +0300

    updated background theme, Features.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import ProductCard from "@/components/Card";
 import CategoryCard from "@/components/CategoryCard";
 import Features from "@/components/Featurers";
 import Hero from "@/components/Hero";
-import React from "react";
 export default function Home() {
 
   //TODO: COMPONENTLER ARASINDAKİ BOŞLUKLARI FİXLE
@@ -24,41 +23,36 @@ export default function Home() {
     
   const data = [
     {
-
       imageURL:
-        'https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/cat-1-400x400.jpg',
-      name: 'BEDROOM',
-  
+        "https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/cat-1-400x400.jpg",
+      name: "Tepsiler",
     },
     {
-
       imageURL:
-        'https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/cat-1-400x400.jpg',
-      name: 'HOME',
-  
+        "https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/cat-1-400x400.jpg",
+      name: "Fincan Takımı",
     },
     {
-
       imageURL:
-        'https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/cat-1-400x400.jpg',
-      name: 'HOME',
-  
+        "https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/cat-1-400x400.jpg",
+      name: "Kahvaltılık Takımı",
     },
     {
-
       imageURL:
-        'https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/cat-1-400x400.jpg',
-      name: 'HOME',
-  
-    }
-  ]
+        "https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/cat-1-400x400.jpg",
+      name: "Özel Tasarım Ürünler",
+    },
+    {
+      imageURL:
+        "https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/cat-2-300x300.jpg",
+      name: "Doğal Taşlar",
+    },
+  ];
   
 
   return (
     <>
       <Hero />
-
-
       <div
         style={{
           padding: "0.5rem",
@@ -69,23 +63,23 @@ export default function Home() {
       </div>
 
       <div
-  style={{
-    display: "grid",
-    gridTemplateColumns: "repeat(auto-fit, minmax(100px, 1fr))",
-    gridGap: "0.01rem", // Grid boşluğunu istediğiniz değerle değiştirin
-  }}
->
-  {data.map((item) => (
-    <CategoryCard data={item} />
-  ))}
-</div>
-
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit, minmax(100px, 1fr))",
+          gridGap: "0.1rem", // Grid boşluğunu istediğiniz değerle değiştirin
+        }}
+      >
+        {data.map((item) => (
+          <CategoryCard data={item} />
+        ))}
+      </div>
 
       <div
         style={{
           display: "grid",
           gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
           gridGap: "0.2rem",
+          backgroundColor: "#FBCE7B",
         }}
       >
         {products.map((product) => (
